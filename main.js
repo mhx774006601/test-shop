c4570dfa40e91477086d9e4220a130423e763d6d
mhx <153974268@qq.com>
2023-02-09 18:03:39 +0800

    2023.2.9

---
+++ main.js
@@ -0,0 +1,43 @@
+
+// #ifndef VUE3
+import Vue from 'vue'
+import App from './App'
+
+import { $http } from '@escook/request-miniprogram'
+uni.$http = $http
+
+// $http.baseUrl = 'https://www.uinav.com'
+$http.baseUrl = 'https://api-hmugo-web.itheima.net'
+
+//请求拦截器
+$http.beforeRequest = function(options){
+	uni.showLoading({
+		title:"数据加载中..."
+	})
+}
+
+// 响应拦截器
+$http.afterRequest = function(){
+	uni.hideLoading()
+}
+
+Vue.config.productionTip = false
+
+App.mpType = 'app'
+
+const app = new Vue({
+    ...App
+})
+app.$mount()
+// #endif
+
+// #ifdef VUE3
+import { createSSRApp } from 'vue'
+import App from './App.vue'
+export function createApp() {
+  const app = createSSRApp(App)
+  return {
+    app
+  }
+}
+// #endif
